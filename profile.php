3506e0321938ac181761ab7a6470793ee1d1269a
SathiyaS <selan.sathiya16@gmail.com>
2024-05-25 14:42:26 +0800

    added background image and profile image on profile page

--- profile.php
+++ profile.php
@@ -6,7 +6,7 @@
 <style>
     body{
         font-family: tahoma;
-        background-color: #e9ebee;
+        background-color: #d0d8e4;
     }
     #blue-bar{
         height: 50px;
@@ -34,14 +34,30 @@
     }
 
     #top-bar-1 {
-        width: 50%;
+        width: 65%;
         display: flex;
         align-items: center;
         justify-content: space-between;
     }
 
+    #profile-pic {
+        width: 20%;
+        border-radius: 50%;
+        border: solid 3px white;
+    }
+
+    #bg-content {
+        margin-top: -210px;
+        width: 100%;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
+
 </style>
 <body>
+    <!-- Top Bar -->
     <div id="blue-bar">
         <div  id="top-bar">
             <div id="top-bar-1">
@@ -51,6 +67,20 @@
             <div>
                 <img src="images/selfie.jpg" alt="" style="width: 42px; border-radius: 20px;">
             </div>
+        </div>
+    </div>
+
+    <!-- cover -->
+    <div style="width: 800px; margin: auto; background-color:black;">
+        <div style="background-color: white; text-align: center; color: #405d9b;">
+            <img src="images/mountain.jpg" alt="" style="width: 100%;"><br> 
+            <div id="bg-content">
+                <img src="images/selfie.jpg" alt="" id="profile-pic"><br>
+                <span style="color: white; font-size: 24px; ">Mary Watson</span>
+            </div>
+
+            <br>
+            Timeline About Friends Photos Settings
         </div>
     </div>
 </body>
